c6bb8845720e51937c5697388ab6df767dc8a7d7
vladward <vladward13@gmail.com>
2022-10-09 13:09:05 +0300

    add eslint and prettier dependencies, add navigation modules

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "react-native",
+        "prettier",
+        "prettier/react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", "ts", "tsx"] }],
+        "no-use-before-define": ["error", { "variables": false }],
+        "react/prop-types": ["error", {"ignore": ["navigation", "navigation.navigate"]}]
+    }
+}
