2655fb9b845c7ed91276aeef6cc2c3a8d5d672c8
vladward <vladward13@gmail.com>
2022-10-11 00:02:31 +0300

    add Card component, add FlatList with Card components on NowPlaying tab

--- src/screens/NowPlaying.tsx
+++ src/screens/NowPlaying.tsx
@@ -1,9 +1,10 @@
-import { NowPlayingPropsType, useAppNavigation } from '../types/types';
-import { Button, Text, View } from 'react-native';
+import { NowPlayingPropsType } from '../types/types';
+import { FlatList, ScrollView, StyleSheet, View } from 'react-native';
 import { THEME } from '../styles/theme';
+import { Card } from '../components';
 
 export const NowPlaying = ({}: NowPlayingPropsType) => {
-  const navigation = useAppNavigation();
+  // const navigation = useAppNavigation();
   return (
     <View
       style={{
@@ -11,13 +12,36 @@ export const NowPlaying = ({}: NowPlayingPropsType) => {
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: THEME.DARK,
+        borderWidth: 0,
       }}
     >
-      <Text>Home Screen</Text>
-      <Button
-        title="Jump to MyWatchlist"
-        onPress={() => navigation.navigate('MyWatchlist', { screen: 'One' })}
-      />
+      {/*<Button*/}
+      {/*  title="Jump to MyWatchlist"*/}
+      {/*  onPress={() => navigation.navigate('MyWatchlist', { screen: 'One' })}*/}
+      {/*/>*/}
+      <View style={styles.listContainer}>
+        <ScrollView>
+          <View>
+            <FlatList<ListItemType>
+              data={new Array(30).fill('new')}
+              renderItem={(item) => <Card cardData={item} />}
+              keyExtractor={(item, index) => item + index.toString()}
+              numColumns={2}
+            />
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listContainer: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+});
+
+type ListItemType = {};
